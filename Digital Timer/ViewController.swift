0b162ac3d40905cefcd57c73359722f0c9f672dd
D7703_15 <d7703_15@D7703-15ui-iMac.local>
2018-04-09 15:05:27 +0900

    Initial Commit

---
+++ Digital Timer/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  Digital Timer
+//
+//  Created by 김종현 on 2018. 4. 4..
+//  Copyright © 2018년 김종현. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var timerswitch: UISwitch!
+    var myTimer = Timer()
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        timerswitch.setOn(false, animated: true)
+    }
+    
+    @objc func updateTime() {
+        let date = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "HH:mm:ss EEE"
+        timeLabel.text = formatter.string(from: date)
+    }
+    @IBAction func timerStrat(_sender: Any) {
+        if timerswitch.isOn == true {
+            myTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateTime), userInfo: nil,repeats: true)
+            
+        } else {
+            myTimer.invalidate()
+    }
+}
+}
+
